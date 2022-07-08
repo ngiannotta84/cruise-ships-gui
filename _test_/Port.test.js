b8f92b42dd2e5a28972dbd7e733597380688cc56
nicola giannotta <nicolagiannotta84@gmail.com>
2022-07-08 20:15:14 +0100

    Port test file made DRY

--- _test_/Port.test.js
+++ _test_/Port.test.js
@@ -3,8 +3,10 @@ const Port = require ('../src/Port.js');
 describe ("Port",() => {
     describe ('with ports',() => {
         let port;
+        let ship;
         beforeEach (() => {
             port = new Port ("Brighton")
+            ship = {};
         });
 it('can be instantiated',() => {
 expect(new Port()).toBeInstanceOf(Object)});
@@ -12,16 +14,14 @@ it("give a name to the Port",() => {
 expect(port.name).toEqual ("Brighton")
 });
 it ("can add ship",()=>{
-    const ship ={};
     port.addShip(ship);
     expect(port.ships).toContain(ship);
     });
 it('can remove a ship',() => {
         const titanic ={};
-        const queenMary={};
         port.addShip(titanic);
-        port.addShip(queenMary);
-        port.removeShip(queenMary);
+        port.addShip(ship);
+        port.removeShip(ship);
         expect(port.ships).toEqual([titanic]);
         });
     })
